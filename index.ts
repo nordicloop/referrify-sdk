eb93b9e64b21a0281ea10720797374d2c8c55beb
sanath <ramsaanath@gmail.com>
2022-02-18 14:13:02 +0530

    export until function

--- index.ts
+++ index.ts
@@ -1,2 +1,2 @@
 export { ValueToQueryMapping, QueryToValueMapping } from './src/UrlParamsMapping'
-export { addLeadToUrl } from './src/utils'
+export { addLeadToUrl, getLeadFromUrl } from './src/utils'
--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,14 @@
 import { ValueToQueryMapping } from './UrlParamsMapping'
 
-export function addLeadToUrl(originalUrl: string, leadId: string) {
+export function addLeadToUrl(originalUrl: string, leadId: string): string {
   const url = new URL(originalUrl)
-  const leadQuery = ValueToQueryMapping.leadId
-  url.searchParams.append(leadQuery, leadId)
+  const leadIdKey = ValueToQueryMapping.leadId
+  url.searchParams.append(leadIdKey, leadId)
   return url.toString()
+}
+
+export function getLeadFromUrl(originalUrl: string): string {
+  const url = new URL(originalUrl)
+  const leadIdKey = ValueToQueryMapping.leadId
+  return url.searchParams.get(leadIdKey)
 }
